296c05c2498fec2dd775692f0ecae006b8c93ede
Santhiago Degenhardt <santhiago.dg@hotmail.com>
2023-07-17 14:40:46 -0300

    Todolist

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+
+    <section className='
+    bg-black
+    text-white 
+    w-[100vw]
+    min-h-screen 
+    justify-center 
+    flex text-center 
+    flex-col'>
+      <h1 className='
+      hover:text-green-400 
+      text-[25em] 
+      hover:text-[13em]'>
+        Hello
+      </h1>
+    </section>
+  )
+}
