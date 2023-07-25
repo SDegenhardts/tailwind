ace8666d299e7e2ecacf2dcde72112ef5e98ef8a
Santhiago Degenhardt <santhiago.dg@hotmail.com>
2023-07-25 16:38:28 -0300

    Todolist

--- app/db.js
+++ app/db.js
@@ -1,6 +1,6 @@
 import postgres from 'postgres'
 
-const connectionString = "postgresql://postgres:4aoTYUbmtEzJtO0s@db.bdcyskwoklhzxliscigs.supabase.co:5432/postgres"
+const connectionString = "postgresql://postgres:1223344556677889@db.bcvgviarctvvuzjbkpgi.supabase.co:5432/postgres"
 const sql = postgres(connectionString)
 
 export default sql
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,26 @@
 import { todo } from "node:test"
 import sql from "./db.js"
-import postgres from "./db.js"
 
 export default async function Home() {
 
-  const Todo = await sql `select bol, descricao from todolist`
+  const Todo = await sql`select bol, descricao from todolist`
 
 
   return (
 
-    <section className='bg-blacktext-white w-[100vw]min-h-screen justify-center flex text-center flex-col'>
-      <h1 className='hover:text-green-400 text-[25em] hover:text-[13em]'>
-        Hello 
+    <section className='bg-blacktext-white w-[100vw]min-h-screen justify-center flex text-center items-center flex-col text-white'>
+      <h1 className='hover:text-green-400 text-[15em] hover:text-[13em]'>
+        Hello
       </h1>
-      <ul>
-        {Todo.map((p)=> (
-          <li key={p.id}>
-            {p.bol} - {p.descricao}
-          </li>
-        ))}
-      </ul>
+      <div className="bg-white flex justify-center outline-3 p-[2em] text-center w-80 rounded-xl ">
+        <ul>
+          {Todo.map((p) => (
+            <li key={p.id} className='text-black'>
+              {p.bol} - {p.descricao}
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   )
 }
