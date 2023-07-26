6a16a1798d6ad84fa6db78148581547398cbebf7
Santhiago Degenhardt <santhiago.dg@hotmail.com>
2023-07-26 16:41:47 -0300

    Todolist

--- app/db.js
+++ app/db.js
@@ -1,6 +1,6 @@
 import postgres from 'postgres'
 
-const connectionString = "postgresql://postgres:1223344556677889@db.bcvgviarctvvuzjbkpgi.supabase.co:5432/postgres"
+const connectionString = process.env.DATABASE_URL
 const sql = postgres(connectionString)
 
 export default sql
--- app/page.tsx
+++ app/page.tsx
@@ -9,16 +9,24 @@ export default async function Home() {
   return (
 
     <section className='bg-blacktext-white w-[100vw]min-h-screen justify-center flex text-center items-center flex-col text-white'>
-      <h1 className='hover:text-green-400 text-[15em] hover:text-[13em]'>
-        Hello
+      <h1 className='hover:text-green-400 text-[3em] hover:text-[5em] transition duration-300 ease-in-out '>
+        Todolist
       </h1>
-      <div className="bg-white flex justify-center outline-3 p-[2em] text-center w-80 rounded-xl ">
+      <input placeholder="Tarefa" className="mb-6 transition duration-300 ease-in-out  hover:bg-green-400 outline-none flex justify-center p-11 rounded-xl text-black text-[1.4em] "></input>
+      <div className="bg-white hover:bg-green-400 transition duration-300 ease-in-out  flex justify-center outline-3 p-[2em] text-[1.5em] text-left w-120 rounded-xl  ">
         <ul>
+          
           {Todo.map((p) => (
             <li key={p.id} className='text-black'>
-              {p.bol} - {p.descricao}
+              {p.bol} <input type="checkbox" className="w-[0.8em] h-[0.8em] accent-black hover:accent-white transition duration-300 ease-in-out "></input> -{p.descricao}
             </li>
           ))}
+          <div className="text-center flex align-middle items-center justify-center">
+          <button className="text-black p-3 rounded-lg mt-8 mt-0 border-none font-bold tracking-widest uppercase decoration-green-400 transition duration-500 ease-in-out text-xl relative overflow-hidden outline
+           hover:decoration-white hover:outline-white shadow-black hover:text-white hover:decoration-black hover:bg-black
+          "
+          >Delete All</button>
+          </div>
         </ul>
       </div>
     </section>
