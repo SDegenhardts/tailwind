37b3ebda1e8e8b341cd3eec957ed4224f42d4de3
Santhiago Degenhardt <santhiago.dg@hotmail.com>
2023-08-02 13:51:13 -0300

    Todolist

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,28 @@ import Header from "./components/header"
 import Input from "./components/input"
 import Button from "./components/button"
 import Incheck from "./components/incheck"
-import Divtodo from "./components/divtodo"
+// import Divtodo from "./components/todo"
 
-export default function Home() {
+export default async function Home() {
 
-  // const Todo = await sql`select bol, descricao from todolist`
+  const Todo = await sql`select bol, descricao from todolist`
 
 
   return (
 
     <section className='mb-5 mt-2 bg-blacktext-white w-[100vw]min-h-screen justify-center flex text-center items-center flex-col text-white'>
-      <Header/>
-      <Input/>
-      <Divtodo/>
+      <Header />
+      <Input />
+      <div className="bg-white hover:bg-green-400 transition duration-300 ease-in-out  flex justify-center outline-3 p-[2em] text-[1.5em] text-left w-120 rounded-xl  ">
+        <ul>
+          {Todo.map((p) => (
+            <li key={p.id} className='text-black'>
+              {p.bol} <Incheck /> -{p.descricao}
+            </li>
+          ))}
+          <Button />
+        </ul>
+      </div>
     </section>
   )
 }
